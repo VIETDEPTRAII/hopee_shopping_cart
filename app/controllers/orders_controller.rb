25a046be9c6e28be1caa908c9445aeacce74932c
NGUYỄN THANH VIỆT <nguyenthanhviet31031998@gmail.com>
2020-03-19 09:50:16 +0700

    Update list orders history of customer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
 
   before_action :logged_in_customer, only: %i[index show new create show]
+  before_action :correct_order_of_current_customer, only: :show
   before_action :current_cart_empty?, only: %i[new create]
-  # before_action :correct_customer, only: %i[index show]
 
   def index
     if logged_in_customer?
@@ -37,6 +37,7 @@ class OrdersController < ApplicationController
 
   private
 
+  # Strong params of order
   def order_params
     params.require(:order)
           .permit(:name, :email, :phone, :address)
@@ -44,8 +45,21 @@ class OrdersController < ApplicationController
                  total_price: @current_cart.sub_total)
   end
 
+  # Before filters
+
+  # Return status of cart
   def current_cart_empty?
     redirect_to root_path if @current_cart.total_quantity.zero?
   end
 
+  # Confirms the correct order of current customer.
+  def correct_order_of_current_customer
+    @order = Order.find_by(id: params[:id])
+    unless @order.nil?
+      @correct_order_of_current_customer = current_customer
+                                           .orders
+                                           .find_by(id: @order.id)
+    end
+    redirect_to root_path if @correct_order_of_current_customer.nil?
+  end
 end
